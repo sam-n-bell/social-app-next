119d1c1708e7cebc66d67dad3e7e59baebda35df
sambell <bell137@gmail.com>
2021-05-23 18:24:22 -0500

    Forgot to merge app.tsx for themeprovider

--- social-app/pages/_app.tsx
+++ social-app/pages/_app.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import  myTheme from '../theme';
-import { ThemeProvider } from '@material-ui/core';
+import React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import myTheme from "../theme";
+import { ThemeProvider } from "@material-ui/core";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side')
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentElement) {
-      jssStyles.parentElement.removeChild(jssStyles)
+      jssStyles.parentElement.removeChild(jssStyles);
     }
-  }, [])
+  }, []);
 
   return (
     <React.Fragment>
@@ -22,11 +22,10 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={myTheme}>
-      <Component {...pageProps} />
-
-        </ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default App
+export default App;
