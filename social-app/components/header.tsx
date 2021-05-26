b7d6731d1b02aa405f07102e72737a72e5b19ad7
Sam Bell <bell137@gmail.com>
2021-05-25 20:19:24 -0500

    drawer

--- social-app/components/header.tsx
+++ social-app/components/header.tsx
@@ -8,6 +8,10 @@ import {
   Typography,
   Button,
   Box,
+  Drawer,
+  ListItem,
+  ListItemText,
+  List,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 // const {StylePropable, StyleResizable} = Mixins
@@ -24,28 +28,66 @@ const Header: React.FC = () => {
       marginLeft: "auto",
       marginRight: -12,
     },
+    centerDrawerText: {
+      textAlign: "center",
+    },
   }));
 
   const classes = useStyles();
 
+  const [showDrawer, setShowDrawer] = React.useState<boolean>(false);
+
+  const toggleDrawer = (status: boolean) => (event) => {
+    setShowDrawer(status);
+  };
+
+  const drawerList = () => {
+    return (
+      <div style={{ minWidth: 200 }} role="presentation">
+        <List>
+          <ListItem button>
+            <ListItemText className={classes.centerDrawerText}>
+              HELLO PERSON!
+            </ListItemText>
+          </ListItem>
+        </List>
+      </div>
+    );
+  };
+
   return (
     <div className={classes.root}>
+      <Drawer
+        anchor={"left"}
+        open={showDrawer}
+        onClose={toggleDrawer(false)}
+        onBackdropClick={toggleDrawer(false)}
+      >
+        {drawerList()}
+      </Drawer>
       <AppBar position="static" className={classes.bar}>
         <Toolbar>
-          <Box component="div" display={{ md: "none" }} className={classes.loginButton}>
+          <Box
+            component="div"
+            display={{ md: "none" }}
+            className={classes.loginButton}
+          >
             <IconButton
               edge="start"
-              // className={classes.menuButton}
               color="inherit"
               aria-label="menu"
+              onClick={toggleDrawer(true)}
             >
               <MenuIcon />
             </IconButton>
           </Box>
-          <Box component="div" display={{xs: "none", md: "block" }} className={classes.loginButton}>
-            <Button color="inherit">
-              Login
-            </Button>
+          <Box
+            component="div"
+            display={{ xs: "none", md: "block" }}
+            className={classes.loginButton}
+          >
+            <Button color="inherit">Login</Button>
+            {/* <Button color="inherit">Logout</Button> */}
           </Box>
         </Toolbar>
       </AppBar>
