23c9dd3aaae648eda049572fc65c6014d2274d51
Sam Bell <bell137@gmail.com>
2021-06-09 22:38:24 -0500

    selector ex

--- social-app/pages/examples/reduxexample.tsx
+++ social-app/pages/examples/reduxexample.tsx
@@ -1,19 +1,24 @@
 import { Button } from "@material-ui/core";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { incrementCountAction } from "../../store/actions/counter";
+import { RootState } from "../../store/reducers";
+import { CounterState } from "../../store/reducers/counter";
 
 const ReduceExample: React.FC = () => {
   const dispatch = useDispatch();
 
+  const counterState = useSelector<RootState, CounterState>(
+    (state) => state.counter
+  );
+
   const dispatchLogin = () => {
-    console.log(typeof incrementCountAction);
     dispatch(incrementCountAction());
   };
 
   return (
     <>
-      <span>Template Page</span>
+      <span>Template Page {counterState.value}</span>
       <Button variant="contained" color="primary" onClick={dispatchLogin}>
         Test Redux
       </Button>
